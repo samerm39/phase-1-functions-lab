ad1c694381791600dceedcd1c30c937e54343bee
Samer Mobarak <samermobarak888@gmail.com>
2023-08-08 13:01:59 -0400

    functions_lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+//test 1
+function distanceFromHqInBlocks(distance){
+    const hqLocation = 42;
+    return Math.abs(distance - hqLocation);
+
+}
+//test 2
+function distanceFromHqInFeet(distance){
+    const hqLocation = 42;
+    const hqLocationInFeet = 264;
+    return Math.abs(distance - hqLocation) * hqLocationInFeet
+}
+
+
+//test 3
+function distanceTravelledInFeet(start, destination){
+    const feetInBlocks = 264
+    return Math.abs(destination - start) * feetInBlocks
+}
+
+//test 4
+
+function calculatesFarePrice(start, destination){
+    const feetInBlocks = 264
+   const distance = Math.abs(destination - start);
+   const distanceInFeet = distance * feetInBlocks
+    if (distanceInFeet <= 400){
+        return 0;
+    }
+    else if (distanceInFeet <= 2000){
+        return Math.abs(distanceInFeet - 400) * .02
+    }
+    else if (distanceInFeet <= 2500){
+        return 25
+    }
+    else if (distanceInFeet > 2500){
+        return 'cannot travel that far'
+    }
+
+    }
